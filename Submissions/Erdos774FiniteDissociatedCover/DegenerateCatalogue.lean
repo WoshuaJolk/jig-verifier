e7f98e7960dcad76f1b858bba1ccb581a6ecc352
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 02:18:19 -0400

    Submission Submissions.Erdos774FiniteDissociatedCover.DegenerateCatalogue for Erdos774FiniteDissociatedCover
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: a6c54e2d-00e5-4de4-ae84-10bf0f1b48f9

---
+++ Submissions/Erdos774FiniteDissociatedCover/DegenerateCatalogue.lean
@@ -0,0 +1,7 @@
+import Mathlib.Data.Nat.Basic
+
+namespace Submissions.Erdos774FiniteDissociatedCover.DegenerateCatalogue
+
+theorem proof : ∀ n : ℕ, n = n := fun _ => rfl
+
+end Submissions.Erdos774FiniteDissociatedCover.DegenerateCatalogue
