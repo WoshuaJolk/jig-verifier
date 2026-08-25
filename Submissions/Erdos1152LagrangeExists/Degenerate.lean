67d3b4a277845a569cf9e25e9013103c38a94e72
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 01:43:07 -0400

    Submission Submissions.Erdos1152LagrangeExists.Degenerate for Erdos1152LagrangeExists
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: f5eb9b6e-1f01-488d-b541-d393eeea4d66

---
+++ Submissions/Erdos1152LagrangeExists/Degenerate.lean
@@ -0,0 +1,7 @@
+import Mathlib.LinearAlgebra.Lagrange
+
+namespace Submissions.Erdos1152LagrangeExists.Degenerate
+
+theorem proof : True := trivial
+
+end Submissions.Erdos1152LagrangeExists.Degenerate
