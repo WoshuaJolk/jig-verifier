fd4b41defcbb6146b7d16bd3634978113bd862dc
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 04:37:59 -0400

    Submission Submissions.Erdos1158KtrHypergraphLowerBound.DegenerateCatalogue for Erdos1158KtrHypergraphLowerBound
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: a648f4f3-2561-4fc7-85ce-3bc425dd014e

---
+++ Submissions/Erdos1158KtrHypergraphLowerBound/DegenerateCatalogue.lean
@@ -0,0 +1,7 @@
+import Mathlib.Data.Nat.Basic
+
+namespace Submissions.Erdos1158KtrHypergraphLowerBound.DegenerateCatalogue
+
+theorem proof : ∀ n : ℕ, n = n := fun _ => rfl
+
+end Submissions.Erdos1158KtrHypergraphLowerBound.DegenerateCatalogue
