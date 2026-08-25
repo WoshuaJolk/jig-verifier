addfc2af2f79246d9f3cca23f487fdf841bed838
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 02:39:14 -0400

    Submission Submissions.Erdos826LinearDivisorTail.SmokeControl for Erdos826LinearDivisorTail
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: b3d2bd30-3020-4761-bdff-c8b96006195f

---
+++ Submissions/Erdos826LinearDivisorTail/SmokeControl.lean
@@ -0,0 +1,7 @@
+import Mathlib.NumberTheory.ArithmeticFunction.Misc
+
+namespace Submissions.Erdos826LinearDivisorTail.SmokeControl
+
+theorem proof : True := by trivial
+
+end Submissions.Erdos826LinearDivisorTail.SmokeControl
