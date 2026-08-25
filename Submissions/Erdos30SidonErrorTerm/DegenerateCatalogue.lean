7b5bd2b79b6ae5f5f38b972cc92363cc1212bad0
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 03:28:22 -0400

    Submission Submissions.Erdos30SidonErrorTerm.DegenerateCatalogue for Erdos30SidonErrorTerm
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: e560ff9a-9e12-40e7-a004-204540c72c46

---
+++ Submissions/Erdos30SidonErrorTerm/DegenerateCatalogue.lean
@@ -0,0 +1,7 @@
+import Mathlib.Data.Nat.Basic
+
+namespace Submissions.Erdos30SidonErrorTerm.DegenerateCatalogue
+
+theorem proof : ∀ n : ℕ, n = n := fun _ => rfl
+
+end Submissions.Erdos30SidonErrorTerm.DegenerateCatalogue
