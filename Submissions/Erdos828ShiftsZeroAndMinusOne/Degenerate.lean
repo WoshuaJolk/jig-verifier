210c0a2ef22a472357961cc98b8e5f4c4b1b889c
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 00:58:12 -0400

    Submission Submissions.Erdos828ShiftsZeroAndMinusOne.Degenerate for Erdos828ShiftsZeroAndMinusOne
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: be600fa8-303e-4faf-afa8-7539be883818

---
+++ Submissions/Erdos828ShiftsZeroAndMinusOne/Degenerate.lean
@@ -0,0 +1,8 @@
+import Mathlib.Data.Nat.Totient
+import Mathlib.Data.Nat.PrimeFin
+
+namespace Submissions.Erdos828ShiftsZeroAndMinusOne.Degenerate
+
+theorem proof : ∀ n : ℕ, n = n := fun _ => rfl
+
+end Submissions.Erdos828ShiftsZeroAndMinusOne.Degenerate
