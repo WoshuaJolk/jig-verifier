e5f799f8140d0b5d7df693cb216455503c435a03
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 04:35:04 -0400

    Submission Submissions.Erdos1049ParameterSpaceInhabited.FalseHypothesis for Erdos1049ParameterSpaceInhabited
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: 9f71ffd4-26d2-483b-83d2-d34e306f217f

---
+++ Submissions/Erdos1049ParameterSpaceInhabited/FalseHypothesis.lean
@@ -0,0 +1,7 @@
+import Mathlib.Data.Rat.Defs
+
+namespace Submissions.Erdos1049ParameterSpaceInhabited.FalseHypothesis
+
+theorem proof : False → ∃ t : ℚ, t > 1 := False.elim
+
+end Submissions.Erdos1049ParameterSpaceInhabited.FalseHypothesis
