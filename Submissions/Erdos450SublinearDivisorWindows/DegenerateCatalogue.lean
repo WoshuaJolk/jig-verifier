f9f1a4ecfd76fcc97805f9625482b4be460471db
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 01:53:32 -0400

    Submission Submissions.Erdos450SublinearDivisorWindows.DegenerateCatalogue for Erdos450SublinearDivisorWindows
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: ee9d3ca0-98c1-49d3-85d7-dcad5861464c

---
+++ Submissions/Erdos450SublinearDivisorWindows/DegenerateCatalogue.lean
@@ -0,0 +1,7 @@
+import Mathlib.Data.Nat.Basic
+
+namespace Submissions.Erdos450SublinearDivisorWindows.DegenerateCatalogue
+
+theorem proof : ∃ n : ℕ, n = n := ⟨0, rfl⟩
+
+end Submissions.Erdos450SublinearDivisorWindows.DegenerateCatalogue
