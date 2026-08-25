6e77a052456f60059f08ad6bb64b65eef48e2864
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 03:16:02 -0400

    Submission Submissions.Erdos1002AsymptoticDistribution.DegenerateCatalogue for Erdos1002AsymptoticDistribution
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: 06328f6d-eeb8-4d12-9441-d41dce53f04c

---
+++ Submissions/Erdos1002AsymptoticDistribution/DegenerateCatalogue.lean
@@ -0,0 +1,7 @@
+import Mathlib.Data.Nat.Basic
+
+namespace Submissions.Erdos1002AsymptoticDistribution.DegenerateCatalogue
+
+theorem proof : ∀ n : ℕ, n = n := fun _ => rfl
+
+end Submissions.Erdos1002AsymptoticDistribution.DegenerateCatalogue
