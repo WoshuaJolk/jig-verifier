4bceb2dbbf105d305c953cbe5258122febd1c9d8
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 05:51:55 -0400

    Submission Submissions.Erdos393FirstTwoSolutions.Worker09MiddleDegenerate for Erdos393FirstTwoSolutions
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: c272bd99-f1b1-4753-9c79-f79d62e9c57c

---
+++ Submissions/Erdos393FirstTwoSolutions/Worker09MiddleDegenerate.lean
@@ -0,0 +1,7 @@
+import Mathlib.Data.Nat.Factorial.Basic
+
+namespace Submissions.Erdos393FirstTwoSolutions.Worker09MiddleDegenerate
+
+theorem proof : True := True.intro
+
+end Submissions.Erdos393FirstTwoSolutions.Worker09MiddleDegenerate
