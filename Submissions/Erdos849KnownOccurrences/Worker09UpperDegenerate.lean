cedde45da0bb64f629339f6c6fe0618da9444be6
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 04:21:21 -0400

    Submission Submissions.Erdos849KnownOccurrences.Worker09UpperDegenerate for Erdos849KnownOccurrences
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: 9da96ec2-98d0-4f93-aa97-80df22d56cac

---
+++ Submissions/Erdos849KnownOccurrences/Worker09UpperDegenerate.lean
@@ -0,0 +1,7 @@
+import Mathlib.Data.Nat.Choose.Basic
+
+namespace Submissions.Erdos849KnownOccurrences.Worker09UpperDegenerate
+
+theorem proof : True := trivial
+
+end Submissions.Erdos849KnownOccurrences.Worker09UpperDegenerate
