856cb6e77777216d0451d49f22430766daa1a26e
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 05:54:07 -0400

    Submission Submissions.Erdos393OddSquareReduction.Worker09MiddleDegenerate for Erdos393OddSquareReduction
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: 24131d8b-6fa1-46fd-a4bc-10dbb6de01e2

---
+++ Submissions/Erdos393OddSquareReduction/Worker09MiddleDegenerate.lean
@@ -0,0 +1,7 @@
+import Mathlib.Data.Nat.Factorial.Basic
+
+namespace Submissions.Erdos393OddSquareReduction.Worker09MiddleDegenerate
+
+theorem proof : True := True.intro
+
+end Submissions.Erdos393OddSquareReduction.Worker09MiddleDegenerate
