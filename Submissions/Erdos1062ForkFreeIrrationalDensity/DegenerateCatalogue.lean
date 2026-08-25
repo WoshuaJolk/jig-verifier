bb03e114bca3002fb992a4c960f2fca7f768a748
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 03:52:49 -0400

    Submission Submissions.Erdos1062ForkFreeIrrationalDensity.DegenerateCatalogue for Erdos1062ForkFreeIrrationalDensity
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: a7a2f026-4404-4c45-b0a5-e705d12518bb

---
+++ Submissions/Erdos1062ForkFreeIrrationalDensity/DegenerateCatalogue.lean
@@ -0,0 +1,7 @@
+import Mathlib.Data.Nat.Basic
+
+namespace Submissions.Erdos1062ForkFreeIrrationalDensity.DegenerateCatalogue
+
+theorem proof : ∀ n : ℕ, n = n := fun _ => rfl
+
+end Submissions.Erdos1062ForkFreeIrrationalDensity.DegenerateCatalogue
