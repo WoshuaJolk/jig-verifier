b371e075c0926e6357bf10e640bda6bbddc8773a
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 03:22:25 -0400

    Submission Submissions.Erdos18PracticalPolyloglog.DegenerateCatalogue for Erdos18PracticalPolyloglog
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: cd51c70a-67d2-4e5d-880a-ddd96f730a22

---
+++ Submissions/Erdos18PracticalPolyloglog/DegenerateCatalogue.lean
@@ -0,0 +1,7 @@
+import Mathlib.Data.Nat.Basic
+
+namespace Submissions.Erdos18PracticalPolyloglog.DegenerateCatalogue
+
+theorem proof : ∀ n : ℕ, n = n := fun _ => rfl
+
+end Submissions.Erdos18PracticalPolyloglog.DegenerateCatalogue
