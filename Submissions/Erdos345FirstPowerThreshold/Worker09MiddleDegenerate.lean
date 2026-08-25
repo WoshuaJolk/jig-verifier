fd61c8c525ebf872b17f2081951c54eec6fbc860
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 05:43:53 -0400

    Submission Submissions.Erdos345FirstPowerThreshold.Worker09MiddleDegenerate for Erdos345FirstPowerThreshold
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: 3b576f91-33f7-4955-9b18-401e959230b5

---
+++ Submissions/Erdos345FirstPowerThreshold/Worker09MiddleDegenerate.lean
@@ -0,0 +1,7 @@
+import Mathlib.Algebra.BigOperators.Group.Finset.Basic
+
+namespace Submissions.Erdos345FirstPowerThreshold.Worker09MiddleDegenerate
+
+theorem proof : True := True.intro
+
+end Submissions.Erdos345FirstPowerThreshold.Worker09MiddleDegenerate
