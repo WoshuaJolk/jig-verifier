daa6b2c7f1923ea4ba4977b9d207f45a4e486ead
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 03:33:18 -0400

    Submission Submissions.Erdos102RichLinesUnbounded.DegenerateCatalogue for Erdos102RichLinesUnbounded
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: bce0de1f-674e-4ce4-971a-4285716c7bae

---
+++ Submissions/Erdos102RichLinesUnbounded/DegenerateCatalogue.lean
@@ -0,0 +1,7 @@
+import Mathlib.Data.Nat.Basic
+
+namespace Submissions.Erdos102RichLinesUnbounded.DegenerateCatalogue
+
+theorem proof : ∀ n : ℕ, n = n := fun _ => rfl
+
+end Submissions.Erdos102RichLinesUnbounded.DegenerateCatalogue
