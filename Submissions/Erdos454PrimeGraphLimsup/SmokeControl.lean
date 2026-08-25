e7b222e617c5c3ebd8a688ac28f4a8bc76825d40
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 01:41:32 -0400

    Submission Submissions.Erdos454PrimeGraphLimsup.SmokeControl for Erdos454PrimeGraphLimsup
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: 7e4d1c30-b366-46f5-8f21-3632efde80d1

---
+++ Submissions/Erdos454PrimeGraphLimsup/SmokeControl.lean
@@ -0,0 +1,7 @@
+import Mathlib.Data.Nat.Prime.Nth
+
+namespace Submissions.Erdos454PrimeGraphLimsup.SmokeControl
+
+theorem proof : True := by trivial
+
+end Submissions.Erdos454PrimeGraphLimsup.SmokeControl
