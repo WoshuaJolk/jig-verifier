6c239f812407c16f5e0b26312ab274a4de43f48e
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 06:21:16 -0400

    Submission Submissions.Erdos597ArrowTargetMonotone.Worker09MiddleControl for Erdos597ArrowTargetMonotone
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: 8e4dbe7f-4617-4eaf-8acb-8af5706adf00

---
+++ Submissions/Erdos597ArrowTargetMonotone/Worker09MiddleControl.lean
@@ -0,0 +1,7 @@
+import Mathlib.Combinatorics.SimpleGraph.Copy
+
+namespace Submissions.Erdos597ArrowTargetMonotone.Worker09MiddleControl
+
+theorem proof : True := True.intro
+
+end Submissions.Erdos597ArrowTargetMonotone.Worker09MiddleControl
