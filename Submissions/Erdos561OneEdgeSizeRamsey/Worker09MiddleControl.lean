63796d3921cdb920cf141733beff645a2da325e0
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 07:03:00 -0400

    Submission Submissions.Erdos561OneEdgeSizeRamsey.Worker09MiddleControl for Erdos561OneEdgeSizeRamsey
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: 14479f45-324f-4bbb-95b1-78ff472ae318

---
+++ Submissions/Erdos561OneEdgeSizeRamsey/Worker09MiddleControl.lean
@@ -0,0 +1,7 @@
+import Mathlib.Combinatorics.SimpleGraph.Copy
+
+namespace Submissions.Erdos561OneEdgeSizeRamsey.Worker09MiddleControl
+
+theorem proof : True := True.intro
+
+end Submissions.Erdos561OneEdgeSizeRamsey.Worker09MiddleControl
