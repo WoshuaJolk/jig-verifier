39ef639e80f028cb3aba873a27179eee96f07487
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 03:11:33 -0400

    Submission Submissions.Erdos1171OneColor.Control for Erdos1171OneColor
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: e2d74f1f-bd29-433a-96ff-ea414575256e

---
+++ Submissions/Erdos1171OneColor/Control.lean
@@ -0,0 +1,9 @@
+import Mathlib.Data.Finset.Basic
+
+namespace Submissions.Erdos1171OneColor.Control
+
+private abbrev EscapedClaim : Prop := True
+
+theorem proof : EscapedClaim := trivial
+
+end Submissions.Erdos1171OneColor.Control
