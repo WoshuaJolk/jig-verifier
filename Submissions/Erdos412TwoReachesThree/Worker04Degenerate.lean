e75d760c6d9b9bd294844dc7e43a8988e898608f
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 02:29:23 -0400

    Submission Submissions.Erdos412TwoReachesThree.Worker04Degenerate for Erdos412TwoReachesThree
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: f1b9cd62-7121-4d21-bf96-e85c19989bfe

---
+++ Submissions/Erdos412TwoReachesThree/Worker04Degenerate.lean
@@ -0,0 +1,10 @@
+import Mathlib.NumberTheory.ArithmeticFunction.Misc
+
+open ArithmeticFunction.sigma
+
+namespace Submissions.Erdos412TwoReachesThree.Worker04Degenerate
+
+theorem proof : False → (σ 1) 2 = 3 :=
+  False.elim
+
+end Submissions.Erdos412TwoReachesThree.Worker04Degenerate
