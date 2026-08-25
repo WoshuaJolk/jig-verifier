37e62923add77fcadb0c2fd3505bea7c3c66a41b
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 05:50:05 -0400

    Submission Submissions.Erdos811ZeroBoundary.Control for Erdos811ZeroBoundary
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: d6fc078d-f8c5-4466-835a-7ad860666f30

---
+++ Submissions/Erdos811ZeroBoundary/Control.lean
@@ -0,0 +1,9 @@
+import Mathlib.Tactic
+
+namespace Submissions.Erdos811ZeroBoundary.Control
+
+private abbrev EscapedClaim : Prop := True
+
+theorem proof : EscapedClaim := trivial
+
+end Submissions.Erdos811ZeroBoundary.Control
