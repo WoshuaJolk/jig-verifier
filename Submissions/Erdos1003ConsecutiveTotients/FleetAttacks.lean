a83942f061f1eb959284b2a4b679e153c4169e3a
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 02:28:04 -0400

    Submission Submissions.Erdos1003ConsecutiveTotients.FleetAttacks for Erdos1003ConsecutiveTotients
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: a6959d7e-385c-404a-b9ac-9e4ad4be936e

---
+++ Submissions/Erdos1003ConsecutiveTotients/FleetAttacks.lean
@@ -0,0 +1,9 @@
+import Mathlib.Data.Finset.Basic
+
+namespace Submissions.Erdos1003ConsecutiveTotients.FleetAttacks
+
+private abbrev EscapedClaim : Prop := True
+
+theorem definitionalEscape : EscapedClaim := trivial
+
+end Submissions.Erdos1003ConsecutiveTotients.FleetAttacks
