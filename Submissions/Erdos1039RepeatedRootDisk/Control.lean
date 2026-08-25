751cc9f8f0e00183c007bb7456655ad042187b2a
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 06:23:46 -0400

    Submission Submissions.Erdos1039RepeatedRootDisk.Control for Erdos1039RepeatedRootDisk
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: e1e8e740-e827-4a33-af60-379808b0aec1

---
+++ Submissions/Erdos1039RepeatedRootDisk/Control.lean
@@ -0,0 +1,9 @@
+import Mathlib.Tactic
+
+namespace Submissions.Erdos1039RepeatedRootDisk.Control
+
+private abbrev EscapedClaim : Prop := True
+
+theorem proof : EscapedClaim := trivial
+
+end Submissions.Erdos1039RepeatedRootDisk.Control
