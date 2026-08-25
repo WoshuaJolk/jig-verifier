f18ff2f1d2436862c85bb86dab11c66fe9327fa7
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 02:37:45 -0400

    Submission Submissions.Erdos853FinitePrefixHasMissingEvenGap.Worker01RedControl for Erdos853FinitePrefixHasMissingEvenGap
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: 8cb89dd9-69c3-46e7-8125-63b5f4787c4d

---
+++ Submissions/Erdos853FinitePrefixHasMissingEvenGap/Worker01RedControl.lean
@@ -0,0 +1,3 @@
+namespace Submissions.Erdos853FinitePrefixHasMissingEvenGap.Worker01RedControl
+theorem proof : True := trivial
+end Submissions.Erdos853FinitePrefixHasMissingEvenGap.Worker01RedControl
