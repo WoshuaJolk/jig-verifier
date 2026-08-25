2d904e846d5b4a83eb9396791b48c95cc6f26172
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 03:15:04 -0400

    Submission Submissions.Erdos985ThreeHasPrimePrimitiveRoot.Worker01RedControl for Erdos985ThreeHasPrimePrimitiveRoot
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: 159cd95d-fbcd-4e82-a1cc-7e1154dab0bb

---
+++ Submissions/Erdos985ThreeHasPrimePrimitiveRoot/Worker01RedControl.lean
@@ -0,0 +1,5 @@
+namespace Submissions.Erdos985ThreeHasPrimePrimitiveRoot.Worker01RedControl
+
+theorem proof : True := trivial
+
+end Submissions.Erdos985ThreeHasPrimePrimitiveRoot.Worker01RedControl
