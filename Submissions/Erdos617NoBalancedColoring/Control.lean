6144430681eb8223843a526e73d0b5216f19ab73
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 04:22:51 -0400

    Submission Submissions.Erdos617NoBalancedColoring.Control for Erdos617NoBalancedColoring
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: 27e376fd-e4f5-4bac-b720-5568bacba7c3

---
+++ Submissions/Erdos617NoBalancedColoring/Control.lean
@@ -0,0 +1,4 @@
+import Mathlib.Data.Nat.Basic
+namespace Submissions.Erdos617NoBalancedColoring.Control
+theorem vacuousHypothesis : False → True := False.elim
+end Submissions.Erdos617NoBalancedColoring.Control
