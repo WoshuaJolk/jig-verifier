7b85011b3ec6011d27127626dfb0f46800490247
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 05:37:20 -0400

    Submission Submissions.Erdos667ThresholdMonotone.Control for Erdos667ThresholdMonotone
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: 8078f8bd-7423-4264-a863-b38cc9147a1d

---
+++ Submissions/Erdos667ThresholdMonotone/Control.lean
@@ -0,0 +1,9 @@
+import Mathlib.Tactic
+
+namespace Submissions.Erdos667ThresholdMonotone.Control
+
+private abbrev EscapedClaim : Prop := True
+
+theorem proof : EscapedClaim := trivial
+
+end Submissions.Erdos667ThresholdMonotone.Control
