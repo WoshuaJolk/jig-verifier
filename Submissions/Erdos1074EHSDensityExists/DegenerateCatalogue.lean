066737afb4206521881aa346b908c268c6145c23
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 03:57:29 -0400

    Submission Submissions.Erdos1074EHSDensityExists.DegenerateCatalogue for Erdos1074EHSDensityExists
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: 1c4a1ad4-6eef-465a-b0b0-1acc146e1a92

---
+++ Submissions/Erdos1074EHSDensityExists/DegenerateCatalogue.lean
@@ -0,0 +1,7 @@
+import Mathlib.Data.Nat.Basic
+
+namespace Submissions.Erdos1074EHSDensityExists.DegenerateCatalogue
+
+theorem proof : ∀ n : ℕ, n = n := fun _ => rfl
+
+end Submissions.Erdos1074EHSDensityExists.DegenerateCatalogue
