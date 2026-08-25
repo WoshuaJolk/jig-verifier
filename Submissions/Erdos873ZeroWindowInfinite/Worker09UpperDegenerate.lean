05b79af358c22e797b3dbe496b5654b6d6d584b3
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 04:25:26 -0400

    Submission Submissions.Erdos873ZeroWindowInfinite.Worker09UpperDegenerate for Erdos873ZeroWindowInfinite
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: 5bc40459-5c96-490a-9c37-bb594fec30a3

---
+++ Submissions/Erdos873ZeroWindowInfinite/Worker09UpperDegenerate.lean
@@ -0,0 +1,7 @@
+import Mathlib.Data.Set.Card
+
+namespace Submissions.Erdos873ZeroWindowInfinite.Worker09UpperDegenerate
+
+theorem proof : True := trivial
+
+end Submissions.Erdos873ZeroWindowInfinite.Worker09UpperDegenerate
