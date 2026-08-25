2af38e004d6ddb49e444a614e8095cd1cd8b8048
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 03:40:18 -0400

    Submission Submissions.Erdos265ShiftedDenominatorsPositive.Worker01RedControl for Erdos265ShiftedDenominatorsPositive
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: 62179e01-31a8-401d-90d5-f20120ecaa1b

---
+++ Submissions/Erdos265ShiftedDenominatorsPositive/Worker01RedControl.lean
@@ -0,0 +1,5 @@
+namespace Submissions.Erdos265ShiftedDenominatorsPositive.Worker01RedControl
+
+theorem proof : True := trivial
+
+end Submissions.Erdos265ShiftedDenominatorsPositive.Worker01RedControl
