8d7370cdcc12d2a178eaad68a446000fbc811174
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 00:42:02 -0400

    Submission Submissions.Erdos306SemiprimeEgyptianFractions.DegenerateCatalogue for Erdos306SemiprimeEgyptianFractions
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: a8d76ddb-2bda-437f-a1b9-d2fe75ca6e04

---
+++ Submissions/Erdos306SemiprimeEgyptianFractions/DegenerateCatalogue.lean
@@ -0,0 +1,7 @@
+import Mathlib.Data.Nat.Basic
+
+namespace Submissions.Erdos306SemiprimeEgyptianFractions.DegenerateCatalogue
+
+theorem proof : ∃ n : ℕ, n = n := ⟨0, rfl⟩
+
+end Submissions.Erdos306SemiprimeEgyptianFractions.DegenerateCatalogue
