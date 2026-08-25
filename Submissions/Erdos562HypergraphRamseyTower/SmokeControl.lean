c607401c4c310ded70317d40fe4ba3b01ba59075
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 02:08:26 -0400

    Submission Submissions.Erdos562HypergraphRamseyTower.SmokeControl for Erdos562HypergraphRamseyTower
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: 8869128a-8f46-4822-a56f-a42f52f7e4e9

---
+++ Submissions/Erdos562HypergraphRamseyTower/SmokeControl.lean
@@ -0,0 +1,7 @@
+import Mathlib.Data.Finset.Basic
+
+namespace Submissions.Erdos562HypergraphRamseyTower.SmokeControl
+
+theorem proof : True := by trivial
+
+end Submissions.Erdos562HypergraphRamseyTower.SmokeControl
