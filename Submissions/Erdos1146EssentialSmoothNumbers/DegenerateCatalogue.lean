bfdde6e2a9848ae8a38b240ecadd25d9cdb1e1b2
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 04:00:55 -0400

    Submission Submissions.Erdos1146EssentialSmoothNumbers.DegenerateCatalogue for Erdos1146EssentialSmoothNumbers
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: 66955cf6-1f78-4e8b-828c-075d57330c92

---
+++ Submissions/Erdos1146EssentialSmoothNumbers/DegenerateCatalogue.lean
@@ -0,0 +1,7 @@
+import Mathlib.Data.Nat.Basic
+
+namespace Submissions.Erdos1146EssentialSmoothNumbers.DegenerateCatalogue
+
+theorem proof : ∀ n : ℕ, n = n := fun _ => rfl
+
+end Submissions.Erdos1146EssentialSmoothNumbers.DegenerateCatalogue
