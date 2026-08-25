f327deaddbc2ca5dbbe4d10f1464dd46dac38fe2
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 05:08:44 -0400

    Submission Submissions.Erdos830AmicablePairsStructural.Control for Erdos830AmicablePairsStructural
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: 05ff7d17-f06f-4621-9314-29da8cb182a1

---
+++ Submissions/Erdos830AmicablePairsStructural/Control.lean
@@ -0,0 +1,7 @@
+import Mathlib.NumberTheory.ArithmeticFunction.Misc
+
+namespace Submissions.Erdos830AmicablePairsStructural.Control
+
+theorem unrelated : True := True.intro
+
+end Submissions.Erdos830AmicablePairsStructural.Control
