ef67cbd67582a1bf69e1a8b3a64eae84e4635117
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 04:56:16 -0400

    Submission Submissions.Erdos917SixCriticalEdgeDensity.Control for Erdos917SixCriticalEdgeDensity
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: 9cf45e07-d2f7-43f8-bcf8-2449381c0f45

---
+++ Submissions/Erdos917SixCriticalEdgeDensity/Control.lean
@@ -0,0 +1,4 @@
+import Mathlib.Data.Nat.Basic
+namespace Submissions.Erdos917SixCriticalEdgeDensity.Control
+theorem vacuousHypothesis : False → True := False.elim
+end Submissions.Erdos917SixCriticalEdgeDensity.Control
