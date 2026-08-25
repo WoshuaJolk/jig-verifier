1a1d4d9187af6f03106c9bc9372afaf45d67c20e
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 06:24:51 -0400

    Submission Submissions.Erdos1075DenseHypergraphSubgraph.FleetAttacks for Erdos1075DenseHypergraphSubgraph
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: fde69c79-7c3c-4e23-bce1-86a2a68b2702

---
+++ Submissions/Erdos1075DenseHypergraphSubgraph/FleetAttacks.lean
@@ -0,0 +1,10 @@
+import Mathlib.Data.Finset.Basic
+import Mathlib.Tactic
+
+namespace Submissions.Erdos1075DenseHypergraphSubgraph.FleetAttacks
+
+private abbrev EscapedClaim : Prop := True
+
+theorem definitionalEscape : EscapedClaim := trivial
+
+end Submissions.Erdos1075DenseHypergraphSubgraph.FleetAttacks
