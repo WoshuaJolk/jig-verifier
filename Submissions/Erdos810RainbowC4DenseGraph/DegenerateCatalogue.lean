d96b9dc25ebbc5bff434e055fbaf49423d44ed93
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 04:33:18 -0400

    Submission Submissions.Erdos810RainbowC4DenseGraph.DegenerateCatalogue for Erdos810RainbowC4DenseGraph
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: a464e6a4-039e-4a83-aab2-3211b888f1bd

---
+++ Submissions/Erdos810RainbowC4DenseGraph/DegenerateCatalogue.lean
@@ -0,0 +1,7 @@
+import Mathlib.Data.Nat.Basic
+
+namespace Submissions.Erdos810RainbowC4DenseGraph.DegenerateCatalogue
+
+theorem proof : ∀ n : ℕ, n = n := fun _ => rfl
+
+end Submissions.Erdos810RainbowC4DenseGraph.DegenerateCatalogue
