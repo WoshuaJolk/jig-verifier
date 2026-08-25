460f27db55cc78830a052499c71048330dd6ed62
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 04:05:09 -0400

    Submission Submissions.Erdos882DivisibilityAntichainSubsetSums.DegenerateCatalogue for Erdos882DivisibilityAntichainSubsetSums
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: ea22ba02-ce4d-47dc-953b-afd14e7374b4

---
+++ Submissions/Erdos882DivisibilityAntichainSubsetSums/DegenerateCatalogue.lean
@@ -0,0 +1,7 @@
+import Mathlib.Data.Nat.Basic
+
+namespace Submissions.Erdos882DivisibilityAntichainSubsetSums.DegenerateCatalogue
+
+theorem proof : ∀ n : ℕ, n = n := fun _ => rfl
+
+end Submissions.Erdos882DivisibilityAntichainSubsetSums.DegenerateCatalogue
