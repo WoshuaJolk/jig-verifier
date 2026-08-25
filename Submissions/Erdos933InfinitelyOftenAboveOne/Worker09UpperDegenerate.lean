b6f09edcfb832d4e69d94776d154213996354cda
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 04:42:38 -0400

    Submission Submissions.Erdos933InfinitelyOftenAboveOne.Worker09UpperDegenerate for Erdos933InfinitelyOftenAboveOne
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: 0d691a8e-cf24-47b6-a541-a471953a17a1

---
+++ Submissions/Erdos933InfinitelyOftenAboveOne/Worker09UpperDegenerate.lean
@@ -0,0 +1,7 @@
+import Mathlib.NumberTheory.Padics.PadicVal.Basic
+
+namespace Submissions.Erdos933InfinitelyOftenAboveOne.Worker09UpperDegenerate
+
+theorem proof : True := trivial
+
+end Submissions.Erdos933InfinitelyOftenAboveOne.Worker09UpperDegenerate
