a4a568071b2abd82bde3c336bcfe279bf40e21b5
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 00:11:51 -0400

    Submission Submissions.Erdos456InfinitelyManyStrict.Degenerate for Erdos456InfinitelyManyStrict
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: bcf361d3-ab2e-4d32-9f73-1d9a28572d9b

---
+++ Submissions/Erdos456InfinitelyManyStrict/Degenerate.lean
@@ -0,0 +1,7 @@
+import Mathlib.Data.Nat.Totient
+
+namespace Submissions.Erdos456InfinitelyManyStrict.Degenerate
+
+theorem proof : ∃ n : ℕ, n = n := ⟨0, rfl⟩
+
+end Submissions.Erdos456InfinitelyManyStrict.Degenerate
