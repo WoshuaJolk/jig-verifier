8025826b840741703d696935a34536bc345f7fa8
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 04:59:51 -0400

    Submission Submissions.Erdos561OneStarBoundary.Worker09MiddleDegenerate for Erdos561OneStarBoundary
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: 600ecfcc-296d-4dd5-ac37-407c84bcf88b

---
+++ Submissions/Erdos561OneStarBoundary/Worker09MiddleDegenerate.lean
@@ -0,0 +1,7 @@
+import Mathlib.Combinatorics.SimpleGraph.Copy
+
+namespace Submissions.Erdos561OneStarBoundary.Worker09MiddleDegenerate
+
+theorem proof : True := trivial
+
+end Submissions.Erdos561OneStarBoundary.Worker09MiddleDegenerate
