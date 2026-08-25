f850e40c3727ac54cd693d417aaa2899fb00a650
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 04:21:23 -0400

    Submission Submissions.Erdos714KrrExtremalLowerBound.DegenerateCatalogue for Erdos714KrrExtremalLowerBound
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: a97d0be8-0b46-4146-812c-5b6dab1d7d00

---
+++ Submissions/Erdos714KrrExtremalLowerBound/DegenerateCatalogue.lean
@@ -0,0 +1,7 @@
+import Mathlib.Data.Nat.Basic
+
+namespace Submissions.Erdos714KrrExtremalLowerBound.DegenerateCatalogue
+
+theorem proof : ∀ n : ℕ, n = n := fun _ => rfl
+
+end Submissions.Erdos714KrrExtremalLowerBound.DegenerateCatalogue
