b25bf573d60d48c27c81aca65dd3f4fba3f07404
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 00:05:18 -0400

    Submission Submissions.Erdos396BaseCases.Degenerate for Erdos396BaseCases
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: ff923183-c1c8-40d5-a375-48093932e6be

---
+++ Submissions/Erdos396BaseCases/Degenerate.lean
@@ -0,0 +1,7 @@
+import Mathlib.Data.Nat.Choose.Central
+
+namespace Submissions.Erdos396BaseCases.Degenerate
+
+theorem proof : ∃ n : ℕ, n = n := ⟨0, rfl⟩
+
+end Submissions.Erdos396BaseCases.Degenerate
