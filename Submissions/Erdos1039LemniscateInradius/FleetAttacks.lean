1d6c35bf13553dbae1da359eb1723d45c41f4285
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 06:21:38 -0400

    Submission Submissions.Erdos1039LemniscateInradius.FleetAttacks for Erdos1039LemniscateInradius
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: 9b513add-72a1-451c-9ea3-76b7fbba5559

---
+++ Submissions/Erdos1039LemniscateInradius/FleetAttacks.lean
@@ -0,0 +1,10 @@
+import Mathlib.Data.Finset.Basic
+import Mathlib.Tactic
+
+namespace Submissions.Erdos1039LemniscateInradius.FleetAttacks
+
+private abbrev EscapedClaim : Prop := True
+
+theorem definitionalEscape : EscapedClaim := trivial
+
+end Submissions.Erdos1039LemniscateInradius.FleetAttacks
