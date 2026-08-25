d6d051de4c9105f4ce0e56ae8c12f566cf6a85cc
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 03:14:29 -0400

    Submission Submissions.Erdos982ConvexPolygonDistances.SmokeControl for Erdos982ConvexPolygonDistances
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: 1279199a-9d27-4aac-aad7-0ac7d100fc64

---
+++ Submissions/Erdos982ConvexPolygonDistances/SmokeControl.lean
@@ -0,0 +1,7 @@
+import Mathlib.Geometry.Euclidean.Angle.Oriented.Affine
+
+namespace Submissions.Erdos982ConvexPolygonDistances.SmokeControl
+
+theorem proof : True := by trivial
+
+end Submissions.Erdos982ConvexPolygonDistances.SmokeControl
