5748ad7260cf917a488c17ba94e964207fcb4a79
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 03:25:43 -0400

    Submission Submissions.Erdos1150LittlewoodUniformGap.SmokeControl for Erdos1150LittlewoodUniformGap
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: 003e976a-8135-4c07-96f1-ca49a356d56d

---
+++ Submissions/Erdos1150LittlewoodUniformGap/SmokeControl.lean
@@ -0,0 +1,7 @@
+import Mathlib.Analysis.Polynomial.Fourier
+
+namespace Submissions.Erdos1150LittlewoodUniformGap.SmokeControl
+
+theorem proof : True := by trivial
+
+end Submissions.Erdos1150LittlewoodUniformGap.SmokeControl
