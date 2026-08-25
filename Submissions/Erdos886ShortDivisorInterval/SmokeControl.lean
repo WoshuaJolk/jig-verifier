da3004beaa9c91535632bcd9ba5bbbe5b7915d0b
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 03:01:07 -0400

    Submission Submissions.Erdos886ShortDivisorInterval.SmokeControl for Erdos886ShortDivisorInterval
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: 120bd327-fbb9-47bc-9e74-5fb1ba12273b

---
+++ Submissions/Erdos886ShortDivisorInterval/SmokeControl.lean
@@ -0,0 +1,7 @@
+import Mathlib.NumberTheory.Divisors
+
+namespace Submissions.Erdos886ShortDivisorInterval.SmokeControl
+
+theorem proof : True := by trivial
+
+end Submissions.Erdos886ShortDivisorInterval.SmokeControl
