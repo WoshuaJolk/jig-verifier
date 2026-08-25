5febea1bb0da406905d6983ad213301cc08e0e1e
woshuajolk <56688460+woshuajolk@users.noreply.github.com>
2026-08-25 00:52:03 -0400

    Submission Submissions.Erdos342UlamTwinPairs.DegenerateCatalogue for Erdos342UlamTwinPairs
    Submitted by @woshuajolk through the Jig API.
    Jig-Artifact: e1830fb6-42ec-4fc3-b100-5af5f6e56d24

---
+++ Submissions/Erdos342UlamTwinPairs/DegenerateCatalogue.lean
@@ -0,0 +1,7 @@
+import Mathlib.Data.Nat.Basic
+
+namespace Submissions.Erdos342UlamTwinPairs.DegenerateCatalogue
+
+theorem proof : ∀ n : ℕ, n ≤ n := fun _ => le_rfl
+
+end Submissions.Erdos342UlamTwinPairs.DegenerateCatalogue
