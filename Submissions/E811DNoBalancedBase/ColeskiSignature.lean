cdee16b93032a36f8d0ad0c9d58c0502f949d4e7
coleski <38134018+coleski@users.noreply.github.com>
2026-09-10 17:15:23 -0500

    Remove E811 Commons modules blocking verifier builds (#3)

--- Submissions/E811DNoBalancedBase/ColeskiSignature.lean
+++
@@ -1,8 +0,0 @@
-import Commons.ColeskiE811Sig20260909_SignatureDReduction
-import Commons.ColeskiE811Sig20260909_SignatureChecksAssembly
-namespace Submissions.E811DNoBalancedBase.ColeskiSignature
-open ColeskiSignatureChecksAssembly
-theorem proof : ColeskiSubmittedE811DNoBalancedBase.statement :=
-  ColeskiSignatureDReduction.target_from_checks row_checks positivity
-end Submissions.E811DNoBalancedBase.ColeskiSignature
-#print axioms Submissions.E811DNoBalancedBase.ColeskiSignature.proof
