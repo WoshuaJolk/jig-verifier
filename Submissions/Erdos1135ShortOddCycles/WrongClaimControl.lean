16273ee4d9815a3a5764a0e90cb3cf22db384c52
adamjkozak <20746630+adamjkozak@users.noreply.github.com>
2026-09-15 01:14:16 -0400

    Check Submissions.Erdos1135ShortOddCycles.WrongClaimControl for Erdos1135ShortOddCycles
    Submitted by @adamjkozak through the Jig API.
    Jig-Check: 022d4c74-922e-4ae3-afb0-b254df3cde88

---
+++ Submissions/Erdos1135ShortOddCycles/WrongClaimControl.lean
@@ -0,0 +1,4 @@
+import Mathlib.Data.Nat.Totient
+namespace Submissions.Erdos1135ShortOddCycles.WrongClaimControl
+theorem proof : True := by trivial
+end Submissions.Erdos1135ShortOddCycles.WrongClaimControl
