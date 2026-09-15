e491d7f06c0cb8f3556d1b6dde9518400a247586
adamjkozak <20746630+adamjkozak@users.noreply.github.com>
2026-09-15 00:34:28 -0400

    Check Submissions.ErdosStrausShiftObstruction409577.WrongClaimControl for ErdosStrausShiftObstruction409577
    Submitted by @adamjkozak through the Jig API.
    Jig-Check: dd287f9f-e40e-4bd3-9729-d27946612930

---
+++ Submissions/ErdosStrausShiftObstruction409577/WrongClaimControl.lean
@@ -0,0 +1,4 @@
+import Mathlib.Data.Nat.Basic
+namespace Submissions.ErdosStrausShiftObstruction409577.WrongClaimControl
+theorem proof : True := by trivial
+end Submissions.ErdosStrausShiftObstruction409577.WrongClaimControl
